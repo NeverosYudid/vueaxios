218338fc8eb97c1cbe50313d08813598cba1e682
NeverosYudid <2005051@utrivieramaya.edu.mx>
2022-01-24 13:40:41 -0500

    vue-clima-Playa

---
+++ src/views/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="result" class="about">
+    <h1>This is an about page</h1>
+    <p>User ID: {{ result.userId }}</p>
+    <p>Title: {{ result.title }}</p>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  created() {
+    let post = {
+      title: "Example UTRM1 2005051",
+      body: "<p>Example 2005051</p>",
+      userId: 1,
+    };
+    let post_update = {
+      title: "Example UTRM1 2005051",
+      body: "<p>Example 2005051</p>",
+      userId: 1,
+    };
+    axios.get("https://jsonplaceholder.typicode.com/todos/4").then((result) => {
+      this.result = result.data;
+      // console.log(result.data);
+    }),
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", post)
+        .then((result) => {
+          console.log(result);
+        }),
+      axios
+        .put("https://jsonplaceholder.typicode.com/posts/1", post_update)
+        .then((result) => {
+          console.log(result);
+        });
+  },
+};
+</script>
---
+++ src/views/Clima.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="result" class="clima">
+    <body>
+    <div class="div1">
+      <header>
+        <p>
+          <b>{{ result.name }}</b>
+        </p>
+      </header>
+      <img
+        :src="
+          'http://openweathermap.org/img/w/' + result.weather[0].icon + '.png'
+        "
+        alt=" "
+      />
+      <p><b>{{ result.weather[0].description }}</b></p>
+      <p>Temp: {{ result.main.temp }}</p>
+      <p>Degrees: {{ result.wind.deg }}</p>
+    </div>
+    </body>
+  </div>
+</template>
+<style>
+.div1{
+  background-color: rgb(172, 204, 204);
+  font-weight: 100px;
+  width: 300px;
+  height: 300px;
+  text-align: center;
+  display: inline-block;
+}
+</style>
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  created() {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=playa%20del%20carmen&APPID=fa0399aac44f59e263c7dce44971db2b"
+      )
+      .then((result) => {
+        this.result = result.data;
+        console.log(result.data);
+      });
+  },
+};
+</script>
